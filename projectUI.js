9927c8a7af762d1ea3cf67e7e180034ceafba282
nielinjie <nielinjie@gmail.com>
2020-05-22 15:18:21 +0800

    project ui

--- projectUI.js
+++ projectUI.js
@@ -3,39 +3,45 @@ const React = require("react");
 const { useState, useEffect } = React;
 const PropTypes = require("prop-types");
 const { Text, Color, Box } = require("ink");
-const { ProjectCheckTask } = require("@nielinjie/multi-repo/dist/Project");
+const { ProjectCheckTask } = require("@nielinjie/multi-repo");
 
 const ProjectUI = ({ project }) => {
-	const [tasks, setTasks] = useState([]);
-	useEffect(() => {
-		new ProjectCheckTask(project).all().then((re) => {
-			setTasks(re.map((r) => r[0]));
-		});
-	}, []);
-	return (
-		<Box flexDirection="column" paddingBottom={2}>
-			<Box flexDirection="row">
-				<Text bold>{project.name}</Text>
-				<Box paddingLeft={3}>
-					<Text>{project.path}</Text>
-				</Box>
-			</Box>
-			<Box flexDirection="column">
-				{tasks.map((task) => {
-					return (
-						<Box key={task.name}>
-							<Text>{task.check ? "✅" : "⚠️"}</Text>
-							<Text>{task.name}</Text>
-						</Box>
-					);
-				})}
-			</Box>
-		</Box>
-	);
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const pt = new ProjectCheckTask(project);
+    setTasks(pt.tasks);
+    pt.all().then((re) => {
+      setTasks(re.map((r) => r[0]));
+    });
+  }, []);
+  return (
+    <Box flexDirection="column" paddingBottom={2}>
+      <Box flexDirection="row">
+        <Text bold>{project.name}</Text>
+        <Box paddingLeft={3}>
+          <Text>{project.path}</Text>
+        </Box>
+      </Box>
+      <Box flexDirection="column">
+        {tasks.map((task) => {
+          return (
+            <Box key={task.name}>
+              {task.check !== undefined ? (
+                <Text>{task.check ? "✅" : "🔺"}</Text>
+              ) : (
+                <Text>...</Text>
+              )}
+              <Text>{task.name}</Text>
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 };
 
 ProjectUI.propTypes = {
-	project: PropTypes.object.isRequired,
+  project: PropTypes.object.isRequired,
 };
 
 module.exports = ProjectUI;
